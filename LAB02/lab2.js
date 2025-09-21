2adc795426faf89f5b04e4bfd43e6f66136c79fa
Vanshika verma <109541924+vanshikaverma2003@users.noreply.github.com>
2025-09-21 05:02:54 -0400

    Asking the user to input their choice

--- LAB02/lab2.js
+++ LAB02/lab2.js
@@ -3,3 +3,12 @@ const prompt = require('prompt');
 
 // Start the prompt
 prompt.start();
+
+// Ask the user for their choice
+prompt.get(['userSelection'], function (err, result) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    const userSelection = result.userSelection.toUpperCase();
